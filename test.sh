cb96ee662ea443b6bdcf493574d79bf5e3cbb89e
David Bindloss <davidb@strategicdata.com.au>
2018-05-10 10:25:19 +1000

    Test execution rewrite.
    A function being tested can now accept N arguments, with the final argument
    being the expected exit code of the function.
    
    expect <function> [args..N] <expected_exit_code>
    e.g. expect my-fn "taking" n arguments 0
    
    Also rewired the failed tests reporting to
    be a bit smarter looking with echos

--- test.sh
+++ test.sh
@@ -4,41 +4,33 @@ TESTS_RAN=0
 PASSED=()
 FAILED=()
 
-function err() {
-    echo "Expected ${1} to eq ${2}"
-    exit 1
-}
-
 function expect() {
     (( TESTS_RAN++ ))
-    RESULT=$($1 $2)
+		ARGS=(${@:2:${#}-2})
+    RESULT=$($1 $ARGS)
     CALLING=${FUNCNAME[1]}
+		EXPECTED="${@:${#}}"
 
-    if [[ "$RESULT" -eq "$3" ]]; then
-        PASSED+=("$CALLING" "$1" "$2" "$3" "$RESULT")
+    if [[ "$RESULT" -eq "$EXPECTED" ]]; then
+        PASSED+=("$CALLING" "$1" "${ARGS[@]}" "$EXPECTED" "$RESULT")
     else
-        FAILED+=("$CALLING" "$1" "$2" "$3" "$RESULT")
+				ARGS_PP=$(echo "${ARGS[*]}" | sed 's/ /, /g')
+        FAILED+=("\tTEST FUNCTION: $CALLING:\n\t  function $1( $ARGS_PP ) returned "$RESULT" but expected "$EXPECTED"")
     fi
 }
 
 function test-end() {
     # create a report
-
     PASSED_TESTS=$((${#PASSED[@]} / 5 ))
-    FAILED_TESTS=$((${#FAILED[@]} / 5 ))
+    FAILED_TESTS=${#FAILED[@]}
     echo -e "$1 suite results:\n"
     echo "Ran ${TESTS_RAN} tests - ${FAILED_TESTS}/${TESTS_RAN} failed, ${PASSED_TESTS}/${TESTS_RAN} passed"
+
     # Print trace of failed tests
     if [ ${FAILED_TESTS} -gt 0 ]; then
-        for i in $(seq 0 $((${FAILED_TESTS} - 1))); do
-            INDEX=$(($i * 4))
-            TEST_CMD=${FAILED[$INDEX]}
-            CMD=${FAILED[$(($INDEX + 1))]}
-            ARGS=${FAILED[$(($INDEX + 2))]}
-            EXPECTED=${FAILED[$(($INDEX + 3))]}
-            RES=${FAILED[$(($INDEX + 4))]}
-            echo ${TEST_CMD} failed:
-            echo ${CMD} with args \"${ARGS}\" expected exit code ${EXPECTED} but got ${RES}
+				echo -e "Failed tests:\n"
+        for i in "${FAILED[@]}"; do
+            echo -e $i
         done
 				exit 1
     else
