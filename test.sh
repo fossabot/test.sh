d6af6cfe24e50ef090cd1e264b6c90a3a0a85e11
David Bindloss <davidb@strategicdata.com.au>
2018-05-10 10:53:26 +1000

    Basic expect checking as well now
    expect can now to equality checking for basic variables:
    
    expect 0 1
    expect "foo" "foo"
    
    this also means that you can pass arbitrary functions to expect as well
    
    expect $(echo "foo") "foo"

--- test.sh
+++ test.sh
@@ -1,30 +1,46 @@
 #!/usr/bin/env bash
 
 TESTS_RAN=0
-PASSED=()
+PASSED=0
 FAILED=()
 
 function expect() {
     (( TESTS_RAN++ ))
-		ARGS=(${@:2:${#}-2})
-    RESULT=$($1 $ARGS)
-    CALLING=${FUNCNAME[1]}
+		CALLING=${FUNCNAME[1]}
 		EXPECTED="${@:${#}}"
 
-    if [[ "$RESULT" -eq "$EXPECTED" ]]; then
-        PASSED+=("$CALLING" "$1" "${ARGS[@]}" "$EXPECTED" "$RESULT")
-    else
-				ARGS_PP=$(echo "${ARGS[*]}" | sed 's/ /, /g')
-        FAILED+=("\tTEST FUNCTION: $CALLING:\n\t  function $1( $ARGS_PP ) returned "$RESULT" but expected "$EXPECTED"")
-    fi
+		if [ $(type -t ${1}) ]; then
+			ARGS=(${@:2:${#}-2})
+			RESULT=$($1 $ARGS)
+			if [[ "$RESULT" -eq "$EXPECTED" ]]; then
+					(( PASSED++ ))
+					PASSED+=("$CALLING" "$1" "${ARGS[@]}" "$EXPECTED" "$RESULT")
+			else
+					CALLER=($(caller))
+					LINE_NO=${CALLER[0]}
+					FILENAME=$(echo ${CALLER[1]} | rev | cut -d'/' -f1 | rev)
+					FAILED+=("\t$FILENAME: ($CALLING:$LINE_NO) \"$1\" returned $RESULT, expected $EXPECTED")
+			fi
+		else
+			# Assume it's a plain equality check
+			# in this case we will only have two args: Expected and Result
+			if [[ "$1" -eq "$2" ]] || [[ "$1" == "$2" ]]; then
+					(( PASSED++ ))
+			else
+					# Where did the test fail?
+					CALLER=($(caller))
+					LINE_NO=${CALLER[0]}
+					FILENAME=$(echo ${CALLER[1]} | rev | cut -d'/' -f1 | rev)
+					FAILED+=("\t$FILENAME: ($CALLING:$LINE_NO) $1 is not equal to "$EXPECTED"")
+			fi
+		fi	
 }
 
 function test-end() {
     # create a report
-    PASSED_TESTS=$((${#PASSED[@]} / 5 ))
     FAILED_TESTS=${#FAILED[@]}
     echo -e "$1 suite results:\n"
-    echo "Ran ${TESTS_RAN} tests - ${FAILED_TESTS}/${TESTS_RAN} failed, ${PASSED_TESTS}/${TESTS_RAN} passed"
+    echo "Ran ${TESTS_RAN} tests - ${FAILED_TESTS}/${TESTS_RAN} failed, ${PASSED}/${TESTS_RAN} passed"
 
     # Print trace of failed tests
     if [ ${FAILED_TESTS} -gt 0 ]; then
