b2f994050c4b11139c27953fc8db1aed49a50c13
David Bindloss <davidb@strategicdata.com.au>
2018-05-10 14:58:18 +1000

    Formatting of code clean ups.
    Looking to implement a "expect-contains" function.

--- test.sh
+++ test.sh
@@ -5,62 +5,60 @@ PASSED=0
 FAILED=()
 
 function test-start() {
-	STARTTIME="$(date +%s%N)"
+	START_TIME="$(date +%s%N)"
 	echo "started \"$1\" test suite"
 }
 
 function expect() {
     (( TESTS_RAN++ ))
-		CALLING=${FUNCNAME[1]}
-		EXPECTED="${@:${#}}"
+    CALLING=${FUNCNAME[1]}
+    EXPECTED="${@:${#}}"
 
-		if [ $(type -t ${1}) ]; then
-			if [ ${#} -le 2 ]; then
-				EXPECTED=0
-				ARGS=$2
-			else
-				ARGS=(${@:2:${#}-2})
-			fi
-			RESULT=$($1 $ARGS)
-			RESULT_EXIT_CODE=$?	
-			if [[ "$RESULT_EXIT_CODE" -eq "$EXPECTED" ]]; then
-					(( PASSED++ ))
-					printf .
-					PASSED+=("$CALLING" "$1" "${ARGS[@]}" "$EXPECTED" "$RESULT_EXIT_CODE")
-			else
-					CALLER=($(caller))
-					LINE_NO=${CALLER[0]}
-					FILENAME=$(echo ${CALLER[1]} | rev | cut -d'/' -f1 | rev)
-					FAILED+=("\t$FILENAME: ($CALLING:$LINE_NO) \"$1\" returned $RESULT_EXIT_CODE, expected $EXPECTED")
-					printf F
-			fi
-		else
-			if [ ${#} -eq 1 ]; then
-				EXPECTED=''
-			fi
-			# Assume it's a plain equality check
-			# in this case we will only have two args: Expected and Result
-			if [ "$1" -eq "$2" ] 2>/dev/null|| [ "$1" = "$2" ] 2>/dev/null; then
-					(( PASSED++ ))
-					printf .
-			else
-					# Where did the test fail?
-					CALLER=($(caller))
-					LINE_NO=${CALLER[0]}
-					FILENAME=$(echo ${CALLER[1]} | rev | cut -d'/' -f1 | rev)
-					FAILED+=("\t$FILENAME: ($CALLING:$LINE_NO) \"$1\" is not equal to \"$EXPECTED\"")
-					printf F
-			fi
-		fi	
+    if [ $(type -t ${1}) ]; then
+        if [ ${#} -le 2 ]; then
+            EXPECTED=0
+            ARGS=$2
+        else
+            ARGS=(${@:2:${#}-2})
+        fi
+        RESULT=$($1 "$ARGS")
+        RESULT_EXIT_CODE=$?
+        if [[ "$RESULT_EXIT_CODE" -eq "$EXPECTED" ]]; then
+                (( PASSED++ ))
+                printf .
+        else
+                CALLER=($(caller))
+                LINE_NO=${CALLER[0]}
+                FILENAME=$(echo ${CALLER[1]} | rev | cut -d'/' -f1 | rev)
+                FAILED+=("\t$FILENAME: ($CALLING:$LINE_NO) \"$1\" returned $RESULT_EXIT_CODE, expected $EXPECTED")
+                printf F
+        fi
+    else
+        if [ ${#} -eq 1 ]; then
+            EXPECTED=''
+        fi
+        # Assume it's a plain equality check
+        # in this case we will only have two args: Expected and Result
+        if [ "$1" -eq "$2" ] 2>/dev/null|| [ "$1" = "$2" ] 2>/dev/null; then
+                (( PASSED++ ))
+                printf .
+        else
+                # Where did the test fail?
+                CALLER=($(caller))
+                LINE_NO=${CALLER[0]}
+                FILENAME=$(echo ${CALLER[1]} | rev | cut -d'/' -f1 | rev)
+                FAILED+=("\t$FILENAME: ($CALLING:$LINE_NO) \"$1\" is not equal to \"$EXPECTED\"")
+                printf F
+        fi
+    fi
 }
 
 function test-end() {
-		ENDTIME="$(date +%s%N)"
+    END_TIME="$(date +%s%N)"
     # required visible decimal place for seconds (leading zeros if needed)
-    local tests_time="$( \
-        printf "%010d" "$(( ${ENDTIME/%N/000000000} 
-                            - ${STARTTIME/%N/000000000} ))")"  # in ns
-		TIME="${tests_time:0:${#tests_time}-9}.${tests_time:${#tests_time}-9:3}s"
+    TEST_TIME=$(printf "%010d" "$(( ${END_TIME/%N/000000000} - ${START_TIME/%N/000000000} ))") # in ns
+    TIME="${TEST_TIME:0:${#TEST_TIME}-9}.${TEST_TIME:${#TEST_TIME}-9:3}s"
+
     # create a report
     FAILED_TESTS=${#FAILED[@]}
     echo -e "\ncompleted in $TIME\n\"$1\" test suite results\n"
@@ -69,8 +67,8 @@ function test-end() {
     # Print trace of failed tests
     if [ ${FAILED_TESTS} -gt 0 ]; then
 				echo -e "Failed tests:\n"
-        for i in "${FAILED[@]}"; do
-            echo -e $i
+        for ERROR in "${FAILED[@]}"; do
+            echo -e "$ERROR"
         done
 				exit 1
     else
